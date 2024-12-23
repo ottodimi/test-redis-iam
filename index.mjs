1595d888c10b31304278fb03e5a971f35d518819
Dmitry Tyshchenko <dmitry.tyshchenko@toolsforhumanity.com>
2024-12-23 12:11:27 +0200

    Check args

--- index.mjs
+++ index.mjs
@@ -1,6 +1,11 @@
+import assert from "assert";
 import { Cluster } from "ioredis";
 
 const [url, username, password] = process.argv.slice(2);
+
+assert(url);
+assert(username);
+assert(password);
 
 const cluster = new Cluster([url], {
   redisOptions: { username, password, tls: {} },
